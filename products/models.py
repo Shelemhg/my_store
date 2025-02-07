bcfb4f2b8c76293a21909e5e9f0728239930a4b6
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2025-02-06 23:14:40 -0600

    ci: Add models to "products"

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+# ./products/models.py
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Product(models.Model):
+    
+    name = models.CharField(max_length=50)
+    price = models.DecimalField()
+    sku = models.CharField(max_length=10)
+    category = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    image = models.ImageField(
+        blank = True, null = True,
+        upload_to = 'media/products'
+        )
+    
+    discount = models.IntegerField()
+    
+    create_date = models.DateTimeField(
+        default = timezone.now)
+    publish_date = models.DateTimeField(
+        blank = True, null = True)
+    
---
+++ products/urls.py
@@ -0,0 +1,10 @@
+# ./products/urls.py
+
+from django.contrib import admin
+from django.urls import path
+
+from .views import index
+
+urlpatterns = [
+    path('', index),
+]
