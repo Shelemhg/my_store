7f8c09c17e3ed0d958580ef055f56e0b8186d7c7
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2025-02-07 22:34:01 -0600

    ci: Create basic models

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Product(models.Model):
     
     name = models.CharField(max_length=50)
-    price = models.DecimalField()
+    price = models.DecimalField(decimal_places = 2, max_digits = 10)
     sku = models.CharField(max_length=10)
     category = models.CharField(max_length=50)
     brand = models.CharField(max_length=50)
