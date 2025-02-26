7592f331f8b51a9901f8a7875f7a7cb8f7b81798
Shelem H. <shelemhg@hotmail.com>
2025-02-26 08:18:23 -0600

    ci: Add template "list_of_products"

--- my_store/urls.py
+++ my_store/urls.py
@@ -2,8 +2,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('products.urls')),
     path('admin/', admin.site.urls),
-]
+    
+] + static()
